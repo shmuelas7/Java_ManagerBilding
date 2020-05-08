ba628695fa83e5bf4c5caabbfd8dd79d00e4c43d
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2020-05-08 17:53:19 +0300

    Add files via upload

---
+++ home_work_5/cooke.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class car
+    {
+       private int speed;
+
+        public cooke(int speed)
+        {
+            this.speed = speed;
+        }
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        public void up()
+        {
+            this.speed++;
+        }
+        public void dowen()
+        {
+            this.speed--;
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter speed car");
+            int x = int.Parse(Console.ReadLine());
+            car car1 = new car(x);
+            Console.WriteLine("if you  wont up speed prees 1 if you wont dowen speed prees 0");
+            int s = int.Parse(Console.ReadLine());
+            if (s)
+            {
+                car1.up();
+            }
+            else
+            {
+                car1.dowen();
+            }
+
+            Console.WriteLine($"speed={car1.Speed}");
+        }
+    }
+}
